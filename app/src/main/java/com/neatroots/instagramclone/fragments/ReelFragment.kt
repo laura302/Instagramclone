a59396c4079c0a91b900f664a2c0b4507b3a5092
Laura <lauracontreras19102003@gmail.com>
2023-12-13 19:10:39 -0600

    Initial commit Instagram Clone 01

---
+++ app/src/main/java/com/neatroots/instagramclone/fragments/ReelFragment.kt
@@ -0,0 +1,56 @@
+package com.neatroots.instagramclone.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2.Orientation
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.neatroots.instagramclone.Models.Reel
+import com.neatroots.instagramclone.R
+import com.neatroots.instagramclone.adapers.ReelAdapter
+import com.neatroots.instagramclone.databinding.FragmentReelBinding
+import com.neatroots.instagramclone.utils.REEL
+
+
+class ReelFragment : Fragment() {
+   private lateinit var binding:FragmentReelBinding
+    lateinit var adapter:ReelAdapter
+    var reelList=ArrayList<Reel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding= FragmentReelBinding.inflate(inflater, container, false)
+        adapter= ReelAdapter(requireContext(),reelList)
+        binding.viewPager.adapter=adapter
+        Firebase.firestore.collection(REEL).get().addOnSuccessListener {
+
+            var tempList=ArrayList<Reel>()
+            reelList.clear()
+
+            for (i in it.documents){
+                var reel=i.toObject<Reel>()!!
+                tempList.add(reel)
+            }
+            reelList.addAll(tempList)
+            reelList.reverse()
+            adapter.notifyDataSetChanged()
+        }
+
+        return binding.root
+    }
+
+    companion object {
+
+    }
+}
